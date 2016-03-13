c4dbfbf7bb95644c3b18bc126540c2cb7c0e0d22
George Aristy <george.aristy@gmail.com>
2016-03-13 09:43:37 -0400

    Fixed error in LinearProgressionFacadeTest.constructorMustFailIfVersionsIsEmpty()

--- src/test/java/org/llorllale/liquibasefacade/LinearProgressionFacadeTest.java
+++ src/test/java/org/llorllale/liquibasefacade/LinearProgressionFacadeTest.java
@@ -199,7 +199,7 @@ public class LinearProgressionFacadeTest {
             connection, 
             new ArrayList<>(),
             changesetFileLocator,
-            null
+            resourceAccessorGenerator
     );
   }
 
